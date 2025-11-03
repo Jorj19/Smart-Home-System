3f1d55317cb4e328a7b129ac690421b90aada515
Jorj19 <buneageorge29@gmail.com>
2025-11-03 18:53:02 +0200

    Curatare cod

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main() {
     Sensor bedroomCO2("CO2", 1100.0, "ppm"); // val mare
     Sensor kitchenTemp("Temperatura", 40.0, "C"); // val mare
 
+    livingTemp.updateValue(22.6);
+
     //Creare Camera
     std::cout << "\nCreating 'Living Room' and adding sensors...\n";
     Room livingRoom("Living Room");
