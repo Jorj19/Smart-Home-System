fc398b9d797358fe402d1d277df432ffe57091eb
Jorj19 <buneageorge29@gmail.com>
2025-11-07 18:04:15 +0200

    Bife

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-cppcheck --std=c++17 --enable=all \
+cppcheck --std=c++17 --enable=all --force --error-exitcode=1 src include
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
     -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
